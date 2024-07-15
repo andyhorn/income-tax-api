8f091f44cf35d6e6456f9996ca94629d400bd8e6
Andy Horn <andyjhorn@gmail.com>
2024-07-15 13:10:40 -0700

    Add routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => HomeComponent,
+  },
+  {
+    path: 'register',
+    loadComponent: () => RegisterComponent,
+  },
+];
