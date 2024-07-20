afb47ab266cf51bd30d0a8b2ac1666ef21e634c8
Andy Horn <andyjhorn@gmail.com>
2024-07-19 22:17:02 -0700

    Make TopBar buttons conditional to auth state

--- client/src/app/layouts/top-bar/top-bar.component.ts
+++ client/src/app/layouts/top-bar/top-bar.component.ts
@@ -1,14 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from '../../auth/business/auth.service';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   standalone: true,
-  imports: [NgbNavModule],
+  imports: [NgbNavModule, AsyncPipe, NgIf],
   styles: ['.spacer { flex: 1; }', '.pointer { cursor: pointer; }'],
 })
 export class AppTopBarComponent {
   router = inject(Router);
+  authService = inject(AuthService);
 }
