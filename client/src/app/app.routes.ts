3a773bcee9944431e809f002338cdeee6a1828e0
Andy Horn <andyjhorn@gmail.com>
2024-07-19 22:17:14 -0700

    Add auth guard function

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,19 +1,57 @@
-import { Routes } from '@angular/router';
-import { RegisterComponent } from './register/register.component';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router, Routes } from '@angular/router';
+import { distinctUntilChanged, map } from 'rxjs';
+import { AuthService } from './auth/business/auth.service';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+type AuthRequirement = 'authenticated' | 'unauthenticated';
 
 export const routes: Routes = [
   {
     path: '',
     loadComponent: () => HomeComponent,
+    canActivate: [authGuard()],
   },
   {
     path: 'register',
     loadComponent: () => RegisterComponent,
+    canActivate: [authGuard('unauthenticated')],
   },
   {
     path: 'login',
     loadComponent: () => LoginComponent,
+    canActivate: [authGuard('unauthenticated')],
   },
 ];
+
+export function authGuard(
+  req: AuthRequirement = 'authenticated',
+): CanActivateFn {
+  return (route, state) => {
+    const router = inject(Router);
+    const authService = inject(AuthService);
+
+    return authService.accessToken$.pipe(
+      map((token) => {
+        if (token && req == 'authenticated') {
+          return true;
+        }
+
+        if (!token && req == 'unauthenticated') {
+          return true;
+        }
+
+        if (token && req == 'unauthenticated') {
+          router.navigateByUrl('/');
+          return false;
+        }
+
+        router.navigateByUrl('/login');
+        return false;
+      }),
+      distinctUntilChanged(),
+    );
+  };
+}
