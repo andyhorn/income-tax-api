95fd0f7a7229c813a699c763ed947d0e67fa49e4
Andy Horn <andyjhorn@gmail.com>
2024-07-22 10:07:35 -0700

    Add auto-focus and paste handling

--- client/src/app/verify-email/verify-email.component.ts
+++ client/src/app/verify-email/verify-email.component.ts
@@ -1,19 +1,32 @@
-import { Component, inject } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { NgForOf } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  inject,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
+import { FormArray, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { bootstrapXCircle } from '@ng-icons/bootstrap-icons';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { AuthError } from '../auth/business/auth.error';
 import { AuthService } from '../auth/business/auth.service';
 import { ToastService } from '../shared/toast/toast.service';
 import { buildVerifyEmailForm, VerifyEmailFormKeys } from './verify-email-form';
 
+export const PIN_LENGTH = 6;
+
 @Component({
   selector: 'app-verify-email',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgForOf, NgIconComponent],
+  providers: [provideIcons({ bootstrapXCircle })],
   templateUrl: './verify-email.component.html',
   styleUrl: './verify-email.component.scss',
 })
-export class VerifyEmailComponent {
+export class VerifyEmailComponent implements AfterViewInit {
   private readonly router = inject(Router);
   private readonly authService = inject(AuthService);
   private readonly toastService = inject(ToastService);
@@ -21,10 +34,49 @@ export class VerifyEmailComponent {
   private email?: string;
 
   public readonly formKeys = VerifyEmailFormKeys;
-  public readonly form = buildVerifyEmailForm();
+  public readonly form = buildVerifyEmailForm(PIN_LENGTH);
+
+  @ViewChildren('digit')
+  private readonly inputs!: QueryList<ElementRef<HTMLInputElement>>;
+
+  public get digits(): FormArray<FormControl<string>> {
+    const array = this.form.get(VerifyEmailFormKeys.PIN);
+    return array as FormArray;
+  }
 
   constructor() {
     this.email = this.router.getCurrentNavigation()?.extras.state?.['email'];
+  }
+
+  public ngAfterViewInit(): void {
+    this.focusDigit(0);
+  }
+
+  public onChanged(control: FormControl<string>, index: number): void {
+    if (control.value && index < PIN_LENGTH) {
+      this.focusDigit(index + 1);
+    }
+  }
+
+  public onPaste(event: ClipboardEvent, index: number): void {
+    event.stopPropagation();
+    const data = event.clipboardData?.getData('text/plain');
+
+    if (data) {
+      const pastedDigits = data.split('');
+
+      if (pastedDigits.length != PIN_LENGTH) {
+        this.toastService.show({
+          message: 'Invalid PIN Length',
+          type: 'info',
+        });
+
+        return;
+      }
+
+      this.digits.patchValue(pastedDigits);
+      this.focusDigit(PIN_LENGTH - 1);
+    }
   }
 
   public submit(): void {
@@ -58,5 +110,9 @@ export class VerifyEmailComponent {
           });
         },
       });
+  }
+
+  private focusDigit(index: number): void {
+    this.inputs?.get(index)?.nativeElement.focus();
   }
 }
