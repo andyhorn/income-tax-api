39290e5866b9a01f5e87193f16afa8be082ccac6
Andy Horn <andyjhorn@gmail.com>
2024-07-21 21:27:18 -0700

    Add resend button

--- client/src/app/verify-email/verify-email.component.ts
+++ client/src/app/verify-email/verify-email.component.ts
@@ -1,7 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { AuthService } from '../auth/business/auth.service';
-import { buildVerifyEmailForm, VerifyEmailFormKeys } from './verify-email-form';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthError } from '../auth/business/auth.error';
+import { AuthService } from '../auth/business/auth.service';
+import { ToastService } from '../shared/toast/toast.service';
+import { buildVerifyEmailForm, VerifyEmailFormKeys } from './verify-email-form';
 
 @Component({
   selector: 'app-verify-email',
@@ -11,10 +14,18 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './verify-email.component.scss',
 })
 export class VerifyEmailComponent {
+  private readonly router = inject(Router);
   private readonly authService = inject(AuthService);
+  private readonly toastService = inject(ToastService);
+
+  private email?: string;
 
   public readonly formKeys = VerifyEmailFormKeys;
   public readonly form = buildVerifyEmailForm();
+
+  constructor() {
+    this.email = this.router.getCurrentNavigation()?.extras.state?.['email'];
+  }
 
   public submit(): void {
     this.form.markAllAsTouched();
@@ -22,5 +33,30 @@ export class VerifyEmailComponent {
     if (this.form.valid) {
       console.log(this.form);
     }
+  }
+
+  public resend(): void {
+    if (!this.email) {
+      return;
+    }
+
+    this.authService
+      .resendVerificationCode({
+        email: this.email,
+      })
+      .subscribe({
+        next: () => {
+          this.toastService.show({
+            message: 'PIN re-sent!',
+            type: 'success',
+          });
+        },
+        error: (err: AuthError) => {
+          this.toastService.show({
+            message: 'An error occurred. Please try again.',
+            type: 'danger',
+          });
+        },
+      });
   }
 }
