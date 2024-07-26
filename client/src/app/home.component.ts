e75475461157453de9fb1449247163e35e996960
Andy Horn <andyjhorn@gmail.com>
2024-07-25 17:05:20 -0700

    Add KeysComponent to HomeComponent

--- client/src/app/home.component.ts
+++ client/src/app/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { KeysComponent } from './keys/keys.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [KeysComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
