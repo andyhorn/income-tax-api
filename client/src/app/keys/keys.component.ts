79010ec68bf5d7d788ccc6f228d16e1aa95658ee
Andy Horn <andyjhorn@gmail.com>
2024-08-01 08:55:56 -0700

    Add delete logic to keys component

--- client/src/app/keys/keys.component.ts
+++ client/src/app/keys/keys.component.ts
@@ -1,29 +1,39 @@
 import { AsyncPipe, DatePipe, NgForOf, NgIf } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { KeysClient } from './keys.client';
+import { ToastService } from '../shared/toast/toast.service';
+import { AddKeyDialogComponent } from './dialogs/add-key-dialog/add-key-dialog.component';
+import { DeleteKeyDialogComponent } from './dialogs/delete-key-dialog/delete-key-dialog.component';
 import { ApiKey } from './keys.interface';
 import { KeysService } from './keys.service';
-import { AddKeyDialogComponent } from './dialogs/add-key-dialog/add-key-dialog.component';
 
 @Component({
   selector: 'app-keys',
   standalone: true,
   imports: [AsyncPipe, NgIf, NgForOf, DatePipe],
-  providers: [KeysClient, KeysService],
   templateUrl: './keys.component.html',
   styleUrl: './keys.component.scss',
 })
 export class KeysComponent {
   private readonly modal = inject(NgbModal);
   private readonly service = inject(KeysService);
+  private readonly toastService = inject(ToastService);
 
   public readonly keys$ = this.service.keys$;
 
-  public async addKey(): Promise<void> {
-    await AddKeyDialogComponent.show(this.modal).then((didCreate) => {
-      if (didCreate) {
-        this.service.refresh();
+  public addKey(): void {
+    AddKeyDialogComponent.show(this.modal);
+  }
+
+  public onDelete(key: ApiKey): void {
+    DeleteKeyDialogComponent.show(this.modal).then((confirmed) => {
+      if (confirmed) {
+        this.service.delete(key.id).subscribe(() =>
+          this.toastService.show({
+            message: 'Deleted!',
+            type: 'info',
+          }),
+        );
       }
     });
   }
