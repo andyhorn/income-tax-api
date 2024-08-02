43c0412b1ea9a6612a43961ef68309ea05e6865a
Andy Horn <andyjhorn@gmail.com>
2024-08-02 08:59:09 -0700

    Use popover for "Copied" message

--- client/src/app/keys/dialogs/add-key-dialog/add-key-dialog.component.ts
+++ client/src/app/keys/dialogs/add-key-dialog/add-key-dialog.component.ts
@@ -1,30 +1,33 @@
 import { AsyncPipe, NgIf } from '@angular/common';
-import { Component, ElementRef, inject, ViewChild } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbActiveModal,
+  NgbModal,
+  NgbPopover,
+  NgbPopoverModule,
+} from '@ng-bootstrap/ng-bootstrap';
 import { BehaviorSubject } from 'rxjs';
-import { ToastService } from '../../../shared/toast/toast.service';
 import { KeysService } from '../../keys.service';
 import { AddKeyFormData, AddKeyFormKeys, buildKeyForm } from './add-key-form';
 
 @Component({
   selector: 'add-key-dialog',
   templateUrl: './add-key-dialog.component.html',
-  imports: [ReactiveFormsModule, AsyncPipe, NgIf],
+  imports: [ReactiveFormsModule, AsyncPipe, NgIf, NgbPopoverModule],
   standalone: true,
 })
 export class AddKeyDialogComponent {
   private readonly modal = inject(NgbActiveModal);
   private readonly service = inject(KeysService);
-  private readonly toastService = inject(ToastService);
   private readonly tokenSubject = new BehaviorSubject<string | null>(null);
 
   public readonly form = buildKeyForm();
   public readonly formKeys = AddKeyFormKeys;
   public readonly token$ = this.tokenSubject.asObservable();
 
-  @ViewChild('token')
-  public token?: ElementRef<HTMLParagraphElement>;
+  @ViewChild(NgbPopover)
+  private readonly copiedPopover!: NgbPopover;
 
   public static show(modal: NgbModal): void {
     modal.open(AddKeyDialogComponent);
@@ -51,10 +54,9 @@ export class AddKeyDialogComponent {
 
     if (token) {
       navigator.clipboard.writeText(token).then(() => {
-        this.toastService.show({
-          message: 'Copied!',
-          type: 'info',
-        });
+        this.copiedPopover.open();
+
+        setTimeout(() => this.copiedPopover.close(), 1000);
       });
     }
   }
